17287efc1cd60d5c148d8d0bcf2b1d6a3bb9cb68
padiln <nicholas.padilla@npcomplete.io>
2020-10-08 13:13:30 -0500

    Revert "Trimmed the branch to only the roverrobotics_ros2 sub directory"
    This reverts commit 0828bf963d84e4aebda3c71375bf1e9e7cda7821.

---
+++ rover_simulation/launch/spawn_2WD_rover.launch.py
@@ -0,0 +1,60 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Get the launch directory
+    rover_navigation_dir = get_package_share_directory('rover_navigation')
+    rover_description_dir = get_package_share_directory('rover_simulation')
+
+    # Create the launch configuration variables
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+
+    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+        'use_robot_state_pub',
+        default_value='True',
+        description='Whether to start the robot state publisher')
+
+    urdf = os.path.join(get_package_share_directory(
+        'rover_description'), 'urdf', 'rover.urdf')
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'rover', '-file', urdf],
+        parameters=[{'use_sim_time': use_sim_time}],
+        output='screen')
+
+    start_robot_state_publisher_cmd = Node(
+        condition=IfCondition(use_robot_state_pub),
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=[urdf])
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_use_robot_state_pub_cmd)
+
+    # Add the actions to launch all of the navigation nodes
+    ld.add_action(spawn_entity)
+    ld.add_action(start_robot_state_publisher_cmd)
+
+    return ld
